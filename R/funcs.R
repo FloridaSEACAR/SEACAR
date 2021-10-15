b0c87b2a1dab282b10264b8eb5629de21c23c053
fawda123 <mbafs2012@gmail.com>
2021-10-15 08:20:55 -0400

    adding dirs and files from SECAR2

---
+++ R/funcs.R
@@ -0,0 +1,85 @@
+#' @export
+scrptflt <- function(fls, category){
+  
+  box::use(
+    dplyr[...]
+  )
+  
+  out <- fls %>%
+    filter(category == !!category) %>%
+    mutate(
+      Index = as.character(1:nrow(.)),
+      Link = paste0('[link](https://github.com/FloridaSEACAR/SEACAR2/blob/master/', File, ')')
+      ) %>% 
+    select(Index, File, Link)
+  
+  return(out)
+  
+}
+
+#' @export
+scrptinp <- function(fl){
+  
+  box::use(
+    dplyr[...],
+    here[...]
+  )
+  
+  tmp <- readLines(here(fl))
+  tmp <- grep('read\\.csv', tmp, value = T)
+  
+  out <- sapply(tmp, function(x) x %>% basename %>% gsub('")$', '', .)) %>% 
+    paste(collapse = ', ')
+
+  if(nchar(out) == 0)
+    out <- 'none'
+  
+  return(out)
+  
+}
+
+#' @export
+scrptout <- function(fl){
+  
+  box::use(
+    dplyr[...],
+    here[...]
+  )
+  
+  tmp <- readLines(here(fl))
+  tmp <- grep('^pdf|write\\.csv|^sink\\(', tmp, value = T)
+
+  out <- sapply(tmp, function(x) x %>% strsplit('"') %>% .[[1]] %>% .[grepl('\\.pdf$|\\.csv$|\\.txt$', .)]) %>% 
+    unlist %>% 
+    paste(collapse = ', ')
+  
+  if(nchar(out) == 0)
+    out <- 'none'
+  
+  return(out)
+  
+}
+
+#' @export
+scrptlib <- function(fl){
+  
+  box::use(
+    dplyr[...],
+    here[...]
+  )
+  
+  tmp <- readLines(here(fl))
+  tmp <- grep('^library\\(', tmp, value = T)
+  browser()
+  out <- sapply(tmp, function(x) x %>% gsub('library\\(|\\)', '', .)) %>% 
+    unlist %>% 
+    unique %>% 
+    sort %>% 
+    paste(collapse = ', ')
+  
+  if(nchar(out) == 0)
+    out <- 'none'
+  
+  return(out)
+  
+}
