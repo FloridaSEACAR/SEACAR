a689e7d5f7a7df5cc73db2a3e5b93f01ff24aa89
fawda123 <mbafs2012@gmail.com>
2021-10-15 11:19:30 -0400

    reorg seacarfiles, review of dry tortug coral

--- R/funcs.R
+++ R/funcs.R
@@ -9,7 +9,7 @@ scrptflt <- function(fls, category){
     filter(category == !!category) %>%
     mutate(
       Index = as.character(1:nrow(.)),
-      Link = paste0('[link](https://github.com/FloridaSEACAR/SEACAR2/blob/master/', File, ')')
+      Link = paste0('[link](https://github.com/FloridaSEACAR/SEACAR/blob/master/', File, ')')
       ) %>% 
     select(Index, File, Link)
   
@@ -27,6 +27,7 @@ scrptinp <- function(fl){
   
   tmp <- readLines(here(fl))
   tmp <- grep('read\\.csv', tmp, value = T)
+  tmp <- tmp[!grepl('\\#', tmp)] # remove commented lines
   
   out <- sapply(tmp, function(x) x %>% basename %>% gsub('")$', '', .)) %>% 
     paste(collapse = ', ')
@@ -48,6 +49,7 @@ scrptout <- function(fl){
   
   tmp <- readLines(here(fl))
   tmp <- grep('^pdf|write\\.csv|^sink\\(', tmp, value = T)
+  tmp <- tmp[!grepl('\\#', tmp)] # remove commented lines
 
   out <- sapply(tmp, function(x) x %>% strsplit('"') %>% .[[1]] %>% .[grepl('\\.pdf$|\\.csv$|\\.txt$', .)]) %>% 
     unlist %>% 
